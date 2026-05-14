7e8708d6a74af35f5c63fdbc088e1b05d51101ed
tyab07 <tyab.mujahid@gmail.com>
2026-05-14 17:25:25 +0500

    Simple Calculator (Structure): Add basic class structure and headers

---
+++ Simple Calculator/src/Calculator.java
@@ -0,0 +1,22 @@
+
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+// Author : M.Tayyab 
+public class Calculator extends Frame implements ActionListener {
+
+    public static void main(String[] args) {
+        // Placeholder
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Placeholder
+    }
+}
