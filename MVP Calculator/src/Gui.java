1b17ae542a1a54f03212135dc20864654c2e2a2e
tyab07 <tyab.mujahid@gmail.com>
2026-05-15 00:03:15 +0500

    MVP Calculator (View): Finalize Gui.java with public methods and window listeners

--- MVP Calculator/src/Gui.java
+++ MVP Calculator/src/Gui.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 
+
 public class Gui extends Frame {
     private TextField txt1;
     private Button addBtn, subBtn, mulBtn, divBtn, powBtn, negBtn, factBtn, clearBtn, equalsBtn;
@@ -8,6 +9,7 @@ public class Gui extends Frame {
 
     public Gui() {
         txt1 = new TextField(10);
+
         addBtn = new Button("+"); addBtn.setActionCommand("add");
         subBtn = new Button("-"); subBtn.setActionCommand("subtract");
         mulBtn = new Button("*"); mulBtn.setActionCommand("multiply");
@@ -25,11 +27,49 @@ public class Gui extends Frame {
         }
 
         this.setSize(300, 300);
+        this.setLocation(300, 300);
         this.setLayout(new FlowLayout());
+
         this.add(txt1);
-        for (int i = 0; i <= 9; i++) this.add(digitBtns[i]);
+        for (int i = 0; i <= 9; i++) {
+            this.add(digitBtns[i]);
+        }
+
         this.add(addBtn); this.add(subBtn); this.add(mulBtn);
         this.add(divBtn); this.add(powBtn); this.add(negBtn);
         this.add(factBtn); this.add(clearBtn); this.add(equalsBtn);
+
+        this.addWindowListener(new WindowAdapter() {
+    
+            public void windowClosing(WindowEvent we) {
+                dispose();
+            }
+        });
+    }
+
+    // methods for presenter
+    public String getText() {
+        return txt1.getText();
+    }
+
+    public void setText(String text) {
+        txt1.setText(text);
+    }
+
+    // allow presenter to attach listener
+    public void addActionListener(ActionListener listener) {
+        addBtn.addActionListener(listener);
+        subBtn.addActionListener(listener);
+        mulBtn.addActionListener(listener);
+        divBtn.addActionListener(listener);
+        powBtn.addActionListener(listener);
+        negBtn.addActionListener(listener);
+        factBtn.addActionListener(listener);
+        clearBtn.addActionListener(listener);
+        equalsBtn.addActionListener(listener);
+
+        for (Button b : digitBtns) {
+            b.addActionListener(listener);
+        }
     }
 }
