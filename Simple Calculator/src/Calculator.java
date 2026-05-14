1a1283b20cdebcdefe72ffa300b316d6c3a94c7f
tyab07 <tyab.mujahid@gmail.com>
2026-05-14 17:25:42 +0500

    Simple Calculator (GUI Components): Add UI fields and initial constructor setup

--- Simple Calculator/src/Calculator.java
+++ Simple Calculator/src/Calculator.java
@@ -10,13 +10,36 @@ import java.awt.event.WindowEvent;
 
 // Author : M.Tayyab 
 public class Calculator extends Frame implements ActionListener {
+    private TextField txt1;
+    private Button addBtn, subBtn, mulBtn, divBtn, powBtn, negBtn, factBtn, clearBtn;
+    private Button equalsBtn;
+    private Button[] digitBtns;
+    private String number1;
+    private String operation;
+
+    public Calculator() {
+        txt1 = new TextField(10);
+        addBtn = new Button("+");
+        subBtn = new Button("-");
+        mulBtn = new Button("*");
+        divBtn = new Button("/");
+        powBtn = new Button("^");
+        negBtn = new Button("+/-");
+        factBtn = new Button("!");
+        clearBtn = new Button("C");
+        equalsBtn = new Button("=");
+        digitBtns = new Button[10];
+        for (int i = 0; i <= 9; i++) {
+            digitBtns[i] = new Button(String.valueOf(i));
+        }
+    }
 
     public static void main(String[] args) {
-        // Placeholder
+        Calculator c = new Calculator();
+        c.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // Placeholder
     }
 }
