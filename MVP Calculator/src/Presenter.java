d122f13f8df1405bc08a1d9e6dd852f119dcc4ce
tyab07 <tyab.mujahid@gmail.com>
2026-05-15 00:03:39 +0500

    MVP Calculator (Presenter): Finalize Presenter.java with advanced math and equals logic

--- MVP Calculator/src/Presenter.java
+++ MVP Calculator/src/Presenter.java
@@ -3,6 +3,7 @@ import java.awt.event.*;
 public class Presenter implements ActionListener {
     private final Gui gui;
     private final Calculate calc;
+
     private String number1;
     private String operation;
 
@@ -14,13 +15,66 @@ public class Presenter implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String cmd = e.getActionCommand();
-        if (cmd.length() == 1 && Character.isDigit(cmd.charAt(0))) {
-            gui.setText(gui.getText() + cmd);
-        } else if (cmd.equals("add") || cmd.equals("subtract") || cmd.equals("multiply") || cmd.equals("divide") || cmd.equals("power")) {
-            operation = cmd;
-            number1 = gui.getText();
-            gui.setText("");
+        try {
+            String cmd = e.getActionCommand();
+            switch (cmd) {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    gui.setText(gui.getText() + cmd);
+                    break;
+
+                case "add":
+                case "subtract":
+                case "multiply":
+                case "divide":
+                case "power":
+                    operation = cmd;
+                    number1 = gui.getText();
+                    gui.setText("");
+                    break;
+
+                case "negate":
+                    if (!gui.getText().isEmpty()) {
+                        double val = Double.parseDouble(gui.getText());
+                        gui.setText(String.valueOf(calc.negate(val)));
+                    }
+                    break;
+
+                case "factorial":
+                    if (!gui.getText().isEmpty()) {
+                        int num = Integer.parseInt(gui.getText());
+                        gui.setText(String.valueOf(calc.factorial(num)));
+                    }
+                    break;
+
+                case "clear":
+                    gui.setText("");
+                    break;
+
+                case "equals":
+                    double num1 = Double.parseDouble(number1);
+                    double num2 = Double.parseDouble(gui.getText());
+                    double result = 0;
+
+                    if (operation.equals("add")) result = calc.add(num1, num2);
+                    else if (operation.equals("subtract")) result = calc.subtract(num1, num2);
+                    else if (operation.equals("multiply")) result = calc.multiply(num1, num2);
+                    else if (operation.equals("divide")) result = calc.divide(num1, num2);
+                    else if (operation.equals("power")) result = calc.power(num1, num2);
+
+                    gui.setText(String.valueOf(result));
+                    break;
+            }
+        } catch (Exception ex) {
+            gui.setText("Error");
         }
     }
 }
