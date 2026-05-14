23b1e2ed423e274308462cde70df11ee46e17923
tyab07 <tyab.mujahid@gmail.com>
2026-05-14 17:26:03 +0500

    Simple Calculator (Final): Restore full functional code and finalize project for Day 1

--- Simple Calculator/src/Calculator.java
+++ Simple Calculator/src/Calculator.java
@@ -18,62 +18,138 @@ public class Calculator extends Frame implements ActionListener {
     private String operation;
 
     public Calculator() {
-        // ... (UI Setup code)
         txt1 = new TextField(10);
-        addBtn = new Button("+"); addBtn.setActionCommand("add");
-        subBtn = new Button("-"); subBtn.setActionCommand("subtract");
-        mulBtn = new Button("*"); mulBtn.setActionCommand("multiply");
-        divBtn = new Button("/"); divBtn.setActionCommand("divide");
-        powBtn = new Button("^"); powBtn.setActionCommand("power");
-        negBtn = new Button("+/-"); negBtn.setActionCommand("negate");
-        factBtn = new Button("!"); factBtn.setActionCommand("factorial");
-        clearBtn = new Button("C"); clearBtn.setActionCommand("clear");
-        equalsBtn = new Button("="); equalsBtn.setActionCommand("equals");
+
+        addBtn = new Button("+");
+        addBtn.setActionCommand("add");
+
+        subBtn = new Button("-");
+        subBtn.setActionCommand("subtract");
+
+        mulBtn = new Button("*");
+        mulBtn.setActionCommand("multiply");
+
+        divBtn = new Button("/");
+        divBtn.setActionCommand("divide");
+
+        powBtn = new Button("^");
+        powBtn.setActionCommand("power");
+
+        negBtn = new Button("+/-");
+        negBtn.setActionCommand("negate");
+
+        factBtn = new Button("!");
+        factBtn.setActionCommand("factorial");
+
+        clearBtn = new Button("C");
+        clearBtn.setActionCommand("clear");
+
+        equalsBtn = new Button("=");
+        equalsBtn.setActionCommand("equals");
+
         digitBtns = new Button[10];
         for (int i = 0; i <= 9; i++) {
             digitBtns[i] = new Button(String.valueOf(i));
             digitBtns[i].setActionCommand(String.valueOf(i));
+        }
+
+        this.setSize(300, 300);
+        this.setLocation(300, 300);
+        this.setLayout(new FlowLayout());
+
+        this.add(txt1);
+        for (int i = 0; i <= 9; i++) {
+            this.add(digitBtns[i]);
             digitBtns[i].addActionListener(this);
         }
-        this.setSize(300, 300);
-        this.setLayout(new FlowLayout());
-        this.add(txt1);
-        for (int i = 0; i <= 9; i++) this.add(digitBtns[i]);
-        this.add(addBtn); this.add(subBtn); this.add(mulBtn); this.add(divBtn);
-        this.add(powBtn); this.add(negBtn); this.add(factBtn); this.add(clearBtn);
+
+        this.add(addBtn);
+        this.add(subBtn);
+        this.add(mulBtn);
+        this.add(divBtn);
+        this.add(powBtn);
+        this.add(negBtn);
+        this.add(factBtn);
+        this.add(clearBtn);
         this.add(equalsBtn);
-        addBtn.addActionListener(this); subBtn.addActionListener(this);
-        mulBtn.addActionListener(this); divBtn.addActionListener(this);
-        powBtn.addActionListener(this); negBtn.addActionListener(this);
-        factBtn.addActionListener(this); clearBtn.addActionListener(this);
+
+        addBtn.addActionListener(this);
+        subBtn.addActionListener(this);
+        mulBtn.addActionListener(this);
+        divBtn.addActionListener(this);
+        powBtn.addActionListener(this);
+        negBtn.addActionListener(this);
+        factBtn.addActionListener(this);
+        clearBtn.addActionListener(this);
         equalsBtn.addActionListener(this);
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent we) {
+                dispose();
+            }
+        });
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String cmd = e.getActionCommand();
-        if (Character.isDigit(cmd.charAt(0)) && cmd.length() == 1) {
-            txt1.setText(txt1.getText() + cmd);
-        } else {
-            switch (cmd) {
-                case "add": case "subtract": case "multiply": case "divide": case "power":
-                    operation = cmd;
-                    number1 = txt1.getText();
-                    txt1.setText("");
-                    break;
-                case "equals":
-                    double n1 = Double.parseDouble(number1);
-                    double n2 = Double.parseDouble(txt1.getText());
-                    double res = 0;
-                    if (operation.equals("add")) res = n1 + n2;
-                    else if (operation.equals("subtract")) res = n1 - n2;
-                    else if (operation.equals("multiply")) res = n1 * n2;
-                    else if (operation.equals("divide")) res = n1 / n2;
-                    else if (operation.equals("power")) res = Math.pow(n1, n2);
-                    txt1.setText(String.valueOf(res));
-                    break;
-                case "clear": txt1.setText(""); break;
-            }
+        switch (e.getActionCommand()) {
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                txt1.setText(txt1.getText() + e.getActionCommand());
+                break;
+            case "add":
+            case "subtract":
+            case "multiply":
+            case "divide":
+            case "power":
+                operation = e.getActionCommand();
+                number1 = txt1.getText();
+                txt1.setText("");
+                break;
+            case "negate":
+                if (!txt1.getText().isEmpty()) {
+                    double val = Double.parseDouble(txt1.getText());
+                    txt1.setText(String.valueOf(-val));
+                }
+                break;
+            case "factorial":
+                if (!txt1.getText().isEmpty()) {
+                    int num = Integer.parseInt(txt1.getText());
+                    int fact = 1;
+                    for (int i = 2; i <= num; i++) {
+                        fact *= i;
+                    }
+                    txt1.setText(String.valueOf(fact));
+                }
+                break;
+            case "clear":
+                txt1.setText("");
+                break;
+            case "equals":
+                double num1 = Double.parseDouble(number1);
+                double num2 = Double.parseDouble(txt1.getText());
+                double result = 0;
+                if (operation.equals("add"))
+                    result = num1 + num2;
+                else if (operation.equals("subtract"))
+                    result = num1 - num2;
+                else if (operation.equals("multiply"))
+                    result = num1 * num2;
+                else if (operation.equals("divide"))
+                    result = num1 / num2;
+                else if (operation.equals("power"))
+                    result = Math.pow(num1, num2);
+                txt1.setText(String.valueOf(result));
+                break;
         }
     }
 
@@ -82,3 +158,5 @@ public class Calculator extends Frame implements ActionListener {
         c.setVisible(true);
     }
 }
+
+
