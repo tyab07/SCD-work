f773761fe3174c368a3f1e28a74d6f3472f5a430
tyab07 <tyab.mujahid@gmail.com>
2026-05-14 17:25:53 +0500

    Simple Calculator (Math Logic): Add arithmetic operations and equals logic

--- Simple Calculator/src/Calculator.java
+++ Simple Calculator/src/Calculator.java
@@ -18,6 +18,7 @@ public class Calculator extends Frame implements ActionListener {
     private String operation;
 
     public Calculator() {
+        // ... (UI Setup code)
         txt1 = new TextField(10);
         addBtn = new Button("+"); addBtn.setActionCommand("add");
         subBtn = new Button("-"); subBtn.setActionCommand("subtract");
@@ -28,14 +29,12 @@ public class Calculator extends Frame implements ActionListener {
         factBtn = new Button("!"); factBtn.setActionCommand("factorial");
         clearBtn = new Button("C"); clearBtn.setActionCommand("clear");
         equalsBtn = new Button("="); equalsBtn.setActionCommand("equals");
-        
         digitBtns = new Button[10];
         for (int i = 0; i <= 9; i++) {
             digitBtns[i] = new Button(String.valueOf(i));
             digitBtns[i].setActionCommand(String.valueOf(i));
             digitBtns[i].addActionListener(this);
         }
-
         this.setSize(300, 300);
         this.setLayout(new FlowLayout());
         this.add(txt1);
@@ -43,7 +42,6 @@ public class Calculator extends Frame implements ActionListener {
         this.add(addBtn); this.add(subBtn); this.add(mulBtn); this.add(divBtn);
         this.add(powBtn); this.add(negBtn); this.add(factBtn); this.add(clearBtn);
         this.add(equalsBtn);
-
         addBtn.addActionListener(this); subBtn.addActionListener(this);
         mulBtn.addActionListener(this); divBtn.addActionListener(this);
         powBtn.addActionListener(this); negBtn.addActionListener(this);
@@ -51,16 +49,36 @@ public class Calculator extends Frame implements ActionListener {
         equalsBtn.addActionListener(this);
     }
 
-    public static void main(String[] args) {
-        Calculator c = new Calculator();
-        c.setVisible(true);
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         String cmd = e.getActionCommand();
-        if (cmd.length() == 1 && Character.isDigit(cmd.charAt(0))) {
+        if (Character.isDigit(cmd.charAt(0)) && cmd.length() == 1) {
             txt1.setText(txt1.getText() + cmd);
+        } else {
+            switch (cmd) {
+                case "add": case "subtract": case "multiply": case "divide": case "power":
+                    operation = cmd;
+                    number1 = txt1.getText();
+                    txt1.setText("");
+                    break;
+                case "equals":
+                    double n1 = Double.parseDouble(number1);
+                    double n2 = Double.parseDouble(txt1.getText());
+                    double res = 0;
+                    if (operation.equals("add")) res = n1 + n2;
+                    else if (operation.equals("subtract")) res = n1 - n2;
+                    else if (operation.equals("multiply")) res = n1 * n2;
+                    else if (operation.equals("divide")) res = n1 / n2;
+                    else if (operation.equals("power")) res = Math.pow(n1, n2);
+                    txt1.setText(String.valueOf(res));
+                    break;
+                case "clear": txt1.setText(""); break;
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.setVisible(true);
     }
 }
