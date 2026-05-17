06bf38038e2c0d018dfcc7df1824f6e2fe7051e9
tyab07 <tyab.mujahid@gmail.com>
2026-05-17 11:46:47 +0500

    Library Management (DAL): Add Student Data Access Object

---
+++ Library Management Layered/dal/StudentDAO.java
@@ -0,0 +1,13 @@
+package com.lms.dal;
+
+import java.util.List;
+
+import com.lms.dto.Book;
+
+public class StudentDAO {
+	
+	public List<Book>getBorrowedBooks(String studentId)
+	{
+		return null;
+	}
+}
