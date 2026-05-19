f674f1e79c52cc79fa7cdb1242a0caf137e1ea00
tyab07 <tyab.mujahid@gmail.com>
2026-05-19 23:33:45 +0500

    Commit 10: Main class Full Implementation

--- StudentDATA/StudentData/src/com/sd/main/main.java
+++ StudentDATA/StudentData/src/com/sd/main/main.java
@@ -1,5 +1,15 @@
 package com.sd.main;
 
+import com.sd.al.studentDAO;
+import com.sd.bll.studentBL;
+
 public class main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        // Dependency Injection
+        studentDAO dal = new studentDAO();
+        studentBL bl = new studentBL(dal);
+
+        System.out.println("Total Students: " + bl.countStudents());
+        System.out.println("Average CGPA: " + bl.calcAvgCGPA());
+    }
 }
