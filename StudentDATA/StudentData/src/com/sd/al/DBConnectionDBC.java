0a27fc099530696dd6ac2ffea22d46557a0b915d
tyab07 <tyab.mujahid@gmail.com>
2026-05-19 23:32:47 +0500

    Commit 3: DB Connection Skeleton

---
+++ StudentDATA/StudentData/src/com/sd/al/DBConnectionDBC.java
@@ -0,0 +1,8 @@
+package com.sd.al;
+
+import java.sql.Connection;
+
+public class DBConnectionDBC {
+    public static Connection getConnection() { return null; }
+    public static void closeConnection() {}
+}
