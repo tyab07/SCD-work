882c7ba01435fd4a5b6bdf953778ce15051db6e5
tyab07 <tyab.mujahid@gmail.com>
2026-05-19 23:32:35 +0500

    Commit 2: DTO Full Implementation

--- StudentDATA/StudentData/src/com/sd/dto/studentDTO.java
+++ StudentDATA/StudentData/src/com/sd/dto/studentDTO.java
@@ -1,5 +1,19 @@
 package com.sd.dto;
 
 public class studentDTO {
-    public studentDTO(int id, String rollNo, String name, double cgpa) {}
+    private int id;
+    private String rollNo;
+    private String name;
+    private double cgpa;
+
+    public studentDTO(int id, String rollNo, String name, double cgpa) {
+        this.id = id;
+        this.rollNo = rollNo;
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+
+    public double getCgpa() { return cgpa; }
+    public String getRollNo() { return rollNo; }
+    public String getName() { return name; }
 }
