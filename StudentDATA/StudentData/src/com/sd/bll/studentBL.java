4c33f22db496563ce339fa8bf9cfef80d94a17c0
tyab07 <tyab.mujahid@gmail.com>
2026-05-19 23:33:24 +0500

    Commit 8: BLL Implementation

--- StudentDATA/StudentData/src/com/sd/bll/studentBL.java
+++ StudentDATA/StudentData/src/com/sd/bll/studentBL.java
@@ -1,9 +1,35 @@
 package com.sd.bll;
 
+
+
+
+import java.util.List;
+
 import com.sd.al.studentDAO;
+import com.sd.dto.studentDTO;
 
 public class studentBL {
-    public studentBL(studentDAO studentDAL) {}
-    public int countStudents() { return 0; }
-    public double calcAvgCGPA() { return 0.0; }
+    private final studentDAO studentDAO;
+
+    // Dependency Injection via constructor
+    public studentBL(studentDAO studentDAL) {
+        this.studentDAO = studentDAL;
+    }
+
+    public int countStudents() {
+        List<studentDTO> students = studentDAO.getAllStudents();
+        return students.size();
+    }
+
+    public double calcAvgCGPA() {
+        List<studentDTO> students = studentDAO.getAllStudents();
+        if (students.isEmpty()) return 0.0;
+
+        double total = 0.0;
+        for (studentDTO s : students) {
+            total += s.getCgpa();
+        }
+        return total / students.size();
+    }
 }
+
