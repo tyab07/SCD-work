dd7b6b53c065a62ff753f004b490158e4193822b
tyab07 <tyab.mujahid@gmail.com>
2026-05-19 23:33:09 +0500

    Commit 6: DAO Implementation

--- StudentDATA/StudentData/src/com/sd/al/studentDAO.java
+++ StudentDATA/StudentData/src/com/sd/al/studentDAO.java
@@ -1,8 +1,39 @@
 package com.sd.al;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+
 import com.sd.dto.studentDTO;
 
+
 public class studentDAO {
-    public List<studentDTO> getAllStudents() { return null; }
+    private String url = "jdbc:mysql://localhost:3306/student_database";
+    private String user = "root"; 
+    private String password = ""; 
+
+    public List<studentDTO> getAllStudents() {
+        List<studentDTO> students = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM student")) {
+
+            while (rs.next()) {
+            	studentDTO s = new studentDTO(
+                    rs.getInt("id"),
+                    rs.getString("roll_no"),
+                    rs.getString("name"),
+                    rs.getDouble("cgpa")
+                );
+                students.add(s);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return students;
+    }
 }
