8309a962cfb49f7f1461fd61031eeb12a8b10d75
tyab07 <tyab.mujahid@gmail.com>
2026-05-19 23:33:19 +0500

    Commit 7: BLL Skeleton

---
+++ StudentDATA/StudentData/src/com/sd/bll/studentBL.java
@@ -0,0 +1,9 @@
+package com.sd.bll;
+
+import com.sd.al.studentDAO;
+
+public class studentBL {
+    public studentBL(studentDAO studentDAL) {}
+    public int countStudents() { return 0; }
+    public double calcAvgCGPA() { return 0.0; }
+}
