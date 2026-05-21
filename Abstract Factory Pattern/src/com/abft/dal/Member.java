6155411c9ea5af32b02cac9578bae444c1cad83f
tyab07 <tyab.mujahid@gmail.com>
2026-05-21 17:15:19 +0500

    Add Member model

---
+++ Abstract Factory Pattern/src/com/abft/dal/Member.java
@@ -0,0 +1,17 @@
+package com.abft.dal;
+
+public class Member {
+    private int id;
+    private String name;
+
+    public Member(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+}
