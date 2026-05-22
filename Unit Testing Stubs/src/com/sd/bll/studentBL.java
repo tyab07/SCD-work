a10becf780b93741381abf19a5afdb461e8a41ec
tyab07 <tyab.mujahid@gmail.com>
2026-05-22 10:41:20 +0500

    Add Student Business Logic layer

---
+++ Unit Testing Stubs/src/com/sd/bll/studentBL.java
@@ -0,0 +1,35 @@
+package com.sd.bll;
+
+
+
+
+import java.util.List;
+
+import com.sd.al.StudentDAOStub;
+import com.sd.dto.studentDTO;
+
+public class studentBL {
+    private final StudentDAOStub studentDAO;
+
+   
+    public studentBL(StudentDAOStub studentDAL) {
+        this.studentDAO = studentDAL;
+    }
+
+    public int countStudents() {
+        List<studentDTO> students = studentDAO.getAllStudents();
+        return students.size();
+    }
+
+    public double calcAvgCGPA() {
+        List<studentDTO> students = studentDAO.getAllStudents();
+        if (students.isEmpty()) return 0.0;
+
+        double total = 0.0;
+        for (studentDTO s : students) {
+            total += s.getCgpa();
+        }
+        return total / students.size();
+    }
+}
+
